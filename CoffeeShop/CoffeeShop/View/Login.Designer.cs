3a6112c12cdc9582115dedf65864528328e5eb7d
Ming <minhlaminh01@gmail.com>
2024-09-15 22:10:10 +0700

    Add images

--- CoffeeShop/CoffeeShop/View/Login.Designer.cs
+++ CoffeeShop/CoffeeShop/View/Login.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 700);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
