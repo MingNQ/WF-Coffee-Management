ba51954110b3355e2a3c3fe0e07c4be26e797ce6
Ming <minhlaminh01@gmail.com>
2024-11-07 00:04:56 +0700

    Bases

---
+++ CoffeeShop/CoffeeShop/View/MainFrame/Interfaces/IStaffDetailView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.View.MainFrame.Interfaces
+{
+    internal interface IStaffDetailView
+    {
+    }
+}
